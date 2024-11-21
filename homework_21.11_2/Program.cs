19410f8997b256f923ec798c91ef6824d0ab21f2
huchupuri <squaer120@gmail.com>
2024-11-21 15:45:09 +0300

    Добавьте файлы проекта.

---
+++ homework_21.11_2/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+
+
+namespace tasks
+{
+    public enum GrumpinessLevel
+    {
+        Low,
+        Medium,
+        High
+    }
+    public struct Ded
+    {
+        public string name;
+        public string[] curses;
+        public int strokes = 0;
+        public GrumpinessLevel level;
+        public Ded(string name, GrumpinessLevel level,string[] curses)
+        {
+            this.name = name;
+            this.level = level;
+            this.curses = curses;
+        }
+        public int CountStrokes(params string[] suggstedCurses)
+        {
+            int k = 0;
+            for (int i = 0; i <= suggstedCurses.Length-1; i++)
+            {
+                for (int n = 0; n <= curses.Length-1; n++)
+                {
+                    if (suggstedCurses[i] == curses[n]) k++;
+                }
+            }
+            strokes += k;
+            return k;
+        }
+    }
+
+    class Program
+    {
+        
+        static void DrawNumber(int? number)
+        {
+            switch (number)
+            {
+                case 0:
+                    Console.WriteLine("  ###  \n #   # \n #   # \n #   # \n   #  ");
+                    break;
+                case 1:
+                    Console.WriteLine("   #   \n  ##   \n   #   \n   #   \n  ###  ");
+                    break;
+                case 2:
+                    Console.WriteLine("  ###  \n #   # \n    #  \n  #    \n ###### ");
+                    break;
+                case 3:
+                    Console.WriteLine("  ###  \n #   # \n    #  \n #   # \n  ###  ");
+                    break;
+                case 4:
+                    Console.WriteLine(" #   # \n #   # \n ##### \n     # \n     # ");
+                    break;
+                case 5:
+                    Console.WriteLine(" ##### \n #      \n #####  \n     #  \n #####");
+                    break;
+                case 6:
+                    Console.WriteLine("   ##  \n  #     \n #####  \n #   #  \n  ###   ");
+                    break;
+                case 7:
+                    Console.WriteLine(" ###### \n     #  \n    #   \n    #   \n    #   ");
+                    break;
+                case 8:
+                    Console.WriteLine("  ###  \n #   # \n  ###  \n #   # \n  ###  ");
+                    break;
+                case 9:
+                    Console.WriteLine("  ###  \n #   # \n  ####  \n     #  \n  ###   ");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static int ArraySum(ref int multArray, out int middleArray, params int[] array)
+        {
+            int sum = 0;
+            Span<int> spanArray = new Span<int>(array);
+            foreach (ref int num in spanArray)
+            {
+                sum += num;
+                multArray *= num;
+
+            }
+            middleArray = sum / array.Length;
+            return sum;
+        }
+        static void NumsSwap(int[] array, int firstNum, int secondNum)
+        {
+            sbyte pos1 = 0, pos2 = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == firstNum) pos1 = (sbyte)i;
+                else if (array[i] == secondNum) pos2 = (sbyte)i;
+            }
+            (array[pos1], array[pos2]) = (array[pos2], array[pos1]);
+        }
+
+        static void Task1()
+        {
+            int[] arr = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < 20; i++) arr[i] = rand.Next(100);
+            Console.WriteLine($"{string.Join(' ', arr)}\nвведите 2 разных числа, которые хотите заменить:");
+            bool flagArr;
+            int firstNum = 0, secondNum = 0;
+            do
+            {
+                bool checkFirstNum = int.TryParse(Console.ReadLine(), out firstNum);
+                bool cheackSecondNum = int.TryParse(Console.ReadLine(), out secondNum);
+                flagArr = (checkFirstNum && cheackSecondNum && (firstNum != secondNum));
+                if (flagArr == false) Console.WriteLine("введите 2 разных числа");
+            }
+            while (flagArr == false);
+            NumsSwap(arr, firstNum, secondNum);
+            Console.WriteLine(string.Join(' ', arr));
+        }
+        static void Task2()
+        {
+            Console.WriteLine("задание 2");
+            int multArray = 1;
+
+            int[] array = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray<int>();
+            Console.WriteLine($"сумма масива {ArraySum(ref multArray, out int middleArray, array)} ,произведение массива {multArray}, среднее арифметическое {middleArray}");
+        }
+        static void Task3()
+        {
+            Console.WriteLine("задание 3");
+            string input = String.Empty;
+            int? number = null;
+            bool flagNum = true;
+            while (flagNum)
+            {
+                try
+                {
+                    input = Console.ReadLine();
+                    number = int.Parse(input);
+                }
+                catch (FormatException)
+                {
+                    if (input == "exit" || input == "закрыть") flagNum = false;
+                    else Console.WriteLine("вы ввели не число");
+                    continue;
+                }
+                if (number > 9 || number < 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Clear();
+                    System.Threading.Thread.Sleep(3000);
+                    Console.ResetColor();
+                    Console.Clear();
+                    Console.WriteLine("Ошибка: число должно быть от 0 до 9.");
+                }
+                else DrawNumber(number);
+            }
+        }
+        static void Task4()
+        {
+            Console.WriteLine("задание 4");
+ 
+            Ded[] deds = new Ded[5];
+
+            deds[0] = new Ded("Дед Василий", GrumpinessLevel.Low, new string[] {"Гады", "Твари", "Бабки" });
+            deds[1] = new Ded("Дед Петр", GrumpinessLevel.Medium, new string[] { "Проститутки", "Карабас" });
+            deds[2] = new Ded("Дед Гриша", GrumpinessLevel.High, new string[] { "Гнили", "Скотина", "Наркоман" });
+            deds[3] = new Ded("Дед Федор", GrumpinessLevel.Medium, new string[] { "Ублюдки", "Нафиг" });
+            deds[4] = new Ded("Драма", GrumpinessLevel.High, new string[] { "Боров", "Пакостник", "Чурка" });
+
+            string[] testCurses = new string[] { "Чурка", "Проститутки", "Ублюдки", "Карабас" };
+
+            foreach (Ded ded in deds)
+            {
+                Console.WriteLine($"{ded.name} получил {ded.CountStrokes(testCurses)} синяков");
+            }
+
+        }
+
+        static void Main()
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+        }
+    }
+}
+
---
+++ laba/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace laba
+{
+    class Program
+    {
+        static int Max(int num1, int num2)
+        {
+            return num1 > num2 ? num1 : num2;
+        }
+        static void SwapOptions(ref string first, ref string second) 
+        {
+            (first, second) = (second, first);
+        }
+
+        static bool LoopFactorial(int num, out int resoult)
+        {
+            resoult = 1;
+            bool flag = false;
+            try
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    checked
+                    {
+                        resoult *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                return false; 
+            }
+
+        }
+        static int RecursionFactorial(int num)
+        {
+
+            if (num < 1) return 1;
+            else return (num * RecursionFactorial(num - 1));
+        }
+        static int Nod(int num1, int num2)
+        {
+            while (num1 != 0 && num2 != 0)
+            {
+                if (num1 > num2) num1 = num1% num2;
+                else num2 = num2% num1;
+            }
+            return (num1 + num2);
+        }
+        static int Nod(int num1, int num2, int num3)
+        {
+            return (Nod(Nod(num1, num2), num3));
+        }
+        static int FibonacciSeq(uint n)
+        {
+            if (n <= 1) return 0;
+            if (n == 2) return 1;
+            else return (FibonacciSeq(n - 1) + FibonacciSeq(n - 2));
+        }
+
+        static void Task1()
+        { 
+            bool checkFirst, checkSecond;
+            int num1, num2;
+            do
+            {
+                Console.Write("введите 2 числа");
+                checkFirst = int.TryParse(Console.ReadLine(), out num1);
+                checkSecond = int.TryParse(Console.ReadLine(), out num2);
+            }
+            while (!(checkFirst && checkSecond));
+            Console.WriteLine($"максимальное число: {Max(num1, num2)}");
+            
+        }
+        static void Task2()
+        {
+            Console.Write("введите 1 параметр: ");
+            string firstOpt = Console.ReadLine();
+            Console.Write("введите 2 параметр: ");
+            string secondOpt = Console.ReadLine();
+            SwapOptions(ref firstOpt, ref secondOpt);
+            Console.WriteLine($"{firstOpt} {secondOpt}");
+        }
+
+        static void Task3()
+        {
+            Console.WriteLine("задание 5.3");
+            int numLoop;
+            do
+            {
+                Console.Write("введите число >0: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out numLoop) && (numLoop > 0));
+            if (LoopFactorial(numLoop, out int resoult)) Console.WriteLine($"факториал равен {resoult}");
+            else Console.WriteLine("не обработано");
+        }
+        static void Task4()
+        {
+            int num;
+            do
+            {
+                Console.Write("введите число > 0: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out num) && (num > 0));
+            Console.WriteLine(RecursionFactorial(num));
+        }
+        static void Task5()
+        {
+            Console.WriteLine("Домашнее задание 5.1");
+            bool flag = true;
+            int[] nums = new int[0];
+            do
+            {
+                flag = true;
+                try
+                {
+                    nums = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray<int>();
+                }
+                catch (FormatException) {}
+                finally
+                {
+                    if (nums.Length < 2 || nums.Length > 3)
+                    {
+                        Console.WriteLine("допускается ввод 2 или 3 целых чисел");
+                        flag = false;
+                    }
+
+                }
+            }
+            while (!flag) ;
+            if (nums.Length == 2) Console.WriteLine($"НОД равен {Nod(nums[0], nums[1])}");
+            else Console.WriteLine($"НОД равен {Nod(nums[0], nums[1], nums[2])}");
+        }
+        
+
+        static void Task6()
+        {
+            Console.WriteLine("домашнее задание 5.2");
+            uint n;
+            do
+            {
+                Console.Write("введите n член ряда Фибоначи (>0)");
+            }
+            while (!uint.TryParse(Console.ReadLine(), out n));
+            Console.WriteLine(FibonacciSeq(n));
+
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            Task5();
+            Task6();     
+            
+        }       
+    }
+}
